bfbb11ddfcde6435419d1edc9de3cc0663bfe1f0
Yui Migaki <yuimigaki1221@icloud.com>
2024-09-23 11:06:39 +0800

    init: Set up portfolio for completion

--- config/db.php
+++ config/db.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Database Configuration File
  *
@@ -14,8 +15,7 @@
 return [
     'host' => '127.0.0.1',
     'port' => 3306,
-    'dbname' => 'xxx_php_mvc_jokes',
-    'username' => 'xxx_php_mvc_jokes',
+    'dbname' => 'ym_php_mvc_jokes',
+    'username' => 'ym_php_mvc_jokes',
     'password' => 'Password1234'
 ];
-
